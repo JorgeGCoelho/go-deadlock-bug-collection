195565c9b66203c3c762104c78666459f7c9ab89
Jorge Coelho <jorgegabrielcoelho2@gmail.com>
2022-04-27 14:35:27 +0100

    Improve kubernetes#25331 bug example
    Also generage migo types

--- kubernetes#25331/main.go
+++ kubernetes#25331/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "sync"
+import (
+	"context"
+	"sync"
+)
 
 // When it receives an error (via wc.errChan) it might block trying to send (wc.resultChan) due to no receivers.
+
+const (
+	outgoingBufSize = 100
+)
+
+type watchChan struct {
+	resultChan chan struct{}
+	errChan    chan error
+	ctx        context.Context
+	cancel     context.CancelFunc
+}
+
+func (wc *watchChan) processEvent(s *sync.WaitGroup) {
+	defer s.Done()
+	for {
+		// Receive event
+		event := struct{}{}
+		select {
+		case wc.resultChan <- event:
+		case <-wc.ctx.Done():
+			return
+		}
+	}
+}
 
 // pkg/storage/etcd3/watcher.go:103
 func (wc *watchChan) run() {
@@ -13,13 +40,23 @@ func (wc *watchChan) run() {
 
 	select {
 	case err := <-wc.errChan:
-		errResult := parseError(err)
 		wc.cancel()
-		if errResult != nil {
-			wc.resultChan <- *errResult
+		if err != nil {
+			wc.resultChan <- struct{}{}
 		}
 	case <-wc.ctx.Done():
 	}
 	resultChanWG.Wait()
 	close(wc.resultChan)
+}
+
+func main() {
+	wc := watchChan{
+		resultChan: make(chan struct{}, outgoingBufSize),
+		errChan:    make(chan error, 1),
+	}
+	ctx := context.Background()
+	wc.ctx, wc.cancel = context.WithCancel(ctx)
+
+	wc.run()
 }
